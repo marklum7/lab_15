37e37bad5e4397b136f621fe0a5a4f7bcd753e02
marklum7 <122746275+marklum7@users.noreply.github.com>
2023-06-01 08:16:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.io.ObjectOutputStream;
+import java.util.Objects;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while(true) {
             String coms = in.next();
-            if (coms == "save"){
+            if (Objects.equals(coms, "save")){
                 try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("urav.txt"))) {
                     function urav = new function(in.nextInt());
                     urav.count();
@@ -17,7 +18,7 @@ public class Main {
                 }
 
             }
-            else if (coms == "upload"){
+            else if (Objects.equals(coms, "upload")){
                 try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("urav.txt"))) {
                     function cal = (function)ois.readObject();
                     System.out.println("y=" + cal.y);
